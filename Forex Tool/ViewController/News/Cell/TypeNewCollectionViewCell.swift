cce5e19b80d3d1dab18e86446e984d85fc85dab9
TienNguyen <nhatlongtien229@gmail.com>
2021-09-16 10:38:38 +0700

    done UI

---
+++ Forex Tool/ViewController/News/Cell/TypeNewCollectionViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  TypeNewCollectionViewCell.swift
+//  Forex Tool
+//
+//  Created by Tien Nguyen on 15/09/2021.
+//
+
+import UIKit
+
+class TypeNewCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var greenView: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    func configCell(item:String){
+        self.titleLbl.text = item
+    }
+    override var isSelected: Bool{
+        didSet{
+            if isSelected == true{
+                titleLbl.font = UIFont(name: "Roboto-Bold", size: 14)
+                greenView.isHidden = false
+            }else{
+                titleLbl.font = UIFont(name: "Roboto-Regular", size: 14)
+                greenView.isHidden = true
+            }
+        }
+    }
+}
